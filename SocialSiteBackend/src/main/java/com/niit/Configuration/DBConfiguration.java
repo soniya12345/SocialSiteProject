2a0851dcf04f86162ee2d5321f939901831a2699
GOVIND RAO <soniyabiradar08@gmail.com>
2017-12-28 18:32:47 -0800

    Backend till job module

---
+++ SocialSiteBackend/src/main/java/com/niit/Configuration/DBConfiguration.java
@@ -0,0 +1,58 @@
+package com.niit.Configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.niit.Model.Job;
+import com.niit.Model.User;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfiguration
+{
+	@SuppressWarnings("rawtypes")
+	@Bean
+	public SessionFactory sessionFactory()
+	{
+		LocalSessionFactoryBuilder lsf=	new LocalSessionFactoryBuilder(getDataSource());
+		Properties hibernateProperties=new Properties();
+		
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+		lsf.addProperties(hibernateProperties);
+		
+		Class classes[]=new Class[]{User.class,Job.class};
+	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
+	}
+	
+	@Bean
+	public DataSource getDataSource() 
+	{
+	    BasicDataSource dataSource = new BasicDataSource();
+	    
+	    dataSource.setDriverClassName("oracle.jdbc.OracleDriver");
+	    dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+	    dataSource.setUsername("collaboration");
+	    dataSource.setPassword("admin123");
+	    
+	    return dataSource;
+	}
+	@Bean
+	public HibernateTransactionManager hibTransManagement()
+	{
+		return new HibernateTransactionManager(sessionFactory());
+	}
+
+}
